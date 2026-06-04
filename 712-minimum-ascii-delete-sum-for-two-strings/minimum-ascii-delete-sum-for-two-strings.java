951f716a02056396d4437f6a8ea900843161cfa2
ishan2004-star <ishansinghs2160@gmail.com>
2026-06-04 22:25:24 +0530

    Time: 16 ms (74.74%) | Memory: 46.6 MB (36.34%) - LeetSync

---
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int minimumDeleteSum(String s1, String s2) {
+
+        int n = s1.length();
+        int m = s2.length();
+        
+        int total_ascii = 0;
+
+        for(char ch : s1.toCharArray()){
+            total_ascii += ch;
+        }
+
+        for(char ch : s2.toCharArray()){
+            total_ascii += ch;
+        }
+
+        int dp[][] = new int[n + 1][m + 1];
+
+
+        for(int i = 1; i < n + 1; i++){
+            for(int j = 1; j < m + 1; j++){
+
+                if(s1.charAt(i - 1) == s2.charAt(j - 1)){
+                    dp[i][j] = s1.charAt(i - 1) + dp[i - 1][j - 1];
+                }
+                else{
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return total_ascii - 2 * (dp[n][m]);
+    }
+}
