78e18f0c216aa398558b8e6782103e9126ba5ecd
ishan2004-star <ishansinghs2160@gmail.com>
2026-06-17 00:23:20 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (36.56%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -3,34 +3,21 @@ class Solution {
 
         int n = nums.length;
 
-        int dp[] = new int[n + 2];
-
-        Arrays.fill(dp, 0);
-
+        int pick = 0;
+        int not_pick = 0;
+        int res = 0;
 
         for(int i = n - 1; i >= 0; i--){
-            int pick = nums[i] + dp[i + 2];
-            int not_pick = dp[i + 1];
 
-            dp[i] = Math.max(pick, not_pick);
+            res = Math.max(nums[i] + pick, not_pick);
+
+            pick = not_pick;
+            not_pick = res;
 
         }
         
-        return dp[0];
+        return res;
     }
  
-    public int help(int nums[], int index, int[] dp){
 
-        if(index >= nums.length) return dp[index] = 0;
-
-        if(dp[index] != -1) return dp[index];
-
-        int pick = -1;
-        int not_pick = -1;
-
-        pick = nums[index] + help(nums, index + 2, dp);
-        not_pick = help(nums, index + 1, dp);
-
-        return dp[index] = Math.max(pick, not_pick);
-    }
 }
