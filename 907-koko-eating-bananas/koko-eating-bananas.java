1a88e7bd8544e39550dd124e6aa3c8b34eb9514d
Ishan Singh <ishansinghs2160@gmail.com>
2026-07-03 00:12:38 +0530

    Time: 29 ms (5.24%) | Memory: 47.3 MB (97.65%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+
+        int max = 0;
+
+        Arrays.sort(piles);
+        
+        for(int i = 0; i < piles.length; i++) {
+            max = Math.max(max, piles[i]);
+
+        }
+
+        return search(1, max, h, piles);
+    }
+
+    public int search(int left, int right, int hour, int[] piles){
+
+        while(left <= right){
+
+            int mid = left + (right - left)/2;
+
+            if(canDo(mid, hour, piles)){
+                right = mid - 1;
+            }
+            else{
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+
+    public boolean canDo(int mid, int hour, int[] piles){
+
+        long hours = 0;
+
+        for(int i = piles.length - 1; i >= 0; i--){
+
+            if(piles[i] > mid){
+                hours += (int)Math.ceil((double)piles[i]/mid);
+            }
+            else{
+                hours += i + 1;
+                break;
+            }
+        }
+
+        return hours <= hour;
+    }
+}
