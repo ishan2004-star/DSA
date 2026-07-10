8baf266386fcf45254702739a8c7fdf082cc7ea4
Ishan Singh <ishansinghs2160@gmail.com>
2026-07-10 17:39:06 +0530

    Time: 9 ms (83.48%) | Memory: 43.8 MB (32.93%) - LeetSync

--- 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.java
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.java
@@ -1,34 +1,30 @@
 class Solution {
     Integer[][] dp;
     int mod = 1000000007;
+    int len;
+    int K;
     public int numRollsToTarget(int n, int k, int target) {
 
-        int[][] table = new int[n + 1][k + 1];
-
         dp = new Integer[n + 1][target + 1];
+        len = n + 1;
+        K = k + 1;
 
-        for (int i = 1; i < table.length; i++) {
-            for (int j = 1; j < table[0].length; j++) {
-                table[i][j] = j;
-            }
-        }
-
-        return help(table, target, 1);
+        return help(target, 1);
     }
 
-    public int help(int table[][], int target, int index) {
+    public int help(int target, int index) {
 
         if(target < 0) return 0;
-        if(target == 0 && index == table.length) return 1;
-        if(index == table.length) return 0;
+        if(target == 0 && index == len) return 1;
+        if(index ==len) return 0;
 
         if(dp[index][target] != null) return dp[index][target];
 
         int ways = 0;
 
-        for(int i = 1; i < table[0].length; i++){
+        for(int i = 1; i < K; i++){
 
-            ways = (ways + help(table, target - table[index][i], index + 1))%mod;
+            ways = (ways + help(target - i, index + 1))%mod;
         }
 
         return dp[index][target] = ways;
