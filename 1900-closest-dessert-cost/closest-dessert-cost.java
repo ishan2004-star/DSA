6d596f8d6f3873d5ef280ceb6577e6579ec234fe
Ishan Singh <ishansinghs2160@gmail.com>
2026-07-14 11:59:13 +0530

    Time: 13 ms (53.33%) | Memory: 42.9 MB (50.37%) - LeetSync

---
+++ 1900-closest-dessert-cost/closest-dessert-cost.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int closestCost(int[] baseCosts, int[] toppingCosts, int target) {
+
+        int ans = Integer.MAX_VALUE;
+
+        for (int i = 0; i < baseCosts.length; i++) {
+            int curr = help(toppingCosts, target, 0, baseCosts[i]);
+
+            if (Math.abs(curr - target) < Math.abs(ans - target) ||
+               (Math.abs(curr - target) == Math.abs(ans - target) && curr < ans)) {
+                ans = curr;
+            }
+        }
+
+        return ans;
+    }
+
+    public int help(int nums[], int target, int index, int sum) {
+
+        if (index == nums.length) {
+            return sum;
+        }
+
+        int one = help(nums, target, index + 1, sum + nums[index]);
+        int two = help(nums, target, index + 1, sum + nums[index] * 2);
+        int skip = help(nums, target, index + 1, sum);
+
+        int ele = one;
+        int max = Math.abs(one - target);
+
+        if (Math.abs(two - target) < max ||
+           (Math.abs(two - target) == max && two < ele)) {
+            ele = two;
+            max = Math.abs(two - target);
+        }
+
+        if (Math.abs(skip - target) < max ||
+           (Math.abs(skip - target) == max && skip < ele)) {
+            ele = skip;
+        }
+
+        return ele;
+    }
+}
