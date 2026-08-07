22488bca1fb3d7d11ff1d4ef31f48ef3977ec701
Ishan Singh <ishansinghs2160@gmail.com>
2026-08-07 13:20:12 +0530

    Time: 20 ms (6.93%) | Memory: 47 MB (10.91%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -1,91 +1,67 @@
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        
+        List<List<int[]>> ls = new ArrayList<>();
 
-        int dist[][] = new int[n][k + 2];
+        int[][] dist = new int[n][k + 2];
 
-        for (int[] row : dist)
-            Arrays.fill(row, Integer.MAX_VALUE);
+        for(int i = 0; i < dist.length; i++){
+            Arrays.fill(dist[i], Integer.MAX_VALUE);
+        }
 
-        List<List<Node>> ls = new ArrayList<>();
-
-        for (int i = 0; i < n; i++)
+        for(int i = 0; i < n; i++){
             ls.add(new ArrayList<>());
+        }
 
-        for (int[] flight : flights) {
+        for(int[] flight : flights){
             int u = flight[0];
             int v = flight[1];
-            int w = flight[2];
+            int cost = flight[2];
 
-            ls.get(u).add(new Node(v, w));
-
+            ls.get(u).add(new int[]{v, cost});
         }
 
-        dijktra(ls, dist, src, dst, k);
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> {
+            return a[1] - b[1];
+        });
 
-        int ans = Integer.MAX_VALUE;
+        pq.add(new int[]{src, 0, k + 1});
+        dist[src][k + 1] = 0;
 
-        for (int i = 0; i <= k + 1; i++) {
-            ans = Math.min(ans, dist[dst][i]);
-        }
+        while(!pq.isEmpty()){
+            
+            int[] curr = pq.poll();
 
-        return ans == Integer.MAX_VALUE ? -1 : ans;
+            int node = curr[0];
+            int cost = curr[1];
+            int left = curr[2];
 
-    }
+            if(left == 0) continue;
 
-    public void dijktra(List<List<Node>> ls, int[][] dist, int src, int dst, int k) {
+            if(cost > dist[node][left]) continue;
 
-        Queue<pair> pq = new LinkedList<>();
+            for(int[] arr : ls.get(node)){
+                int neighbor = arr[0];
+                int c = arr[1];
 
-        pq.add(new pair(src, 0, 0));
-        dist[src][0] = 0;
-
-        while (!pq.isEmpty()) {
-
-            pair poll = pq.poll();
-            int node = poll.node;
-            int weight = poll.weight;
-            int stop = poll.stop;
-
-            if (stop > k)
-                continue;
-
-            if (weight > dist[node][stop])
-                continue;
-
-            for (Node n : ls.get(node)) {
-                int N = n.node;
-                int W = n.weight;
-
-                if (stop + 1 <= k + 1 && W + weight < dist[N][stop + 1]) {
-
-                    dist[N][stop + 1] = W + weight;
-
-                    pq.offer(new pair(N, W + weight, stop + 1));
+                if(cost + c < dist[neighbor][left - 1]){
+                    dist[neighbor][left - 1] = cost + c;
+                    pq.add(new int[]{neighbor, dist[neighbor][left - 1], left - 1});
                 }
             }
         }
 
-    }
-}
+        int min = Integer.MAX_VALUE;
 
-class pair {
-    int node;
-    int weight;
-    int stop;
+        for(int i = 0; i <= k; i++){
+            min = Math.min(min, dist[dst][i]);
+        }
 
-    pair(int node, int weight, int stop) {
-        this.node = node;
-        this.weight = weight;
-        this.stop = stop;
-    }
-}
+        if(min == Integer.MAX_VALUE) return -1;
 
-class Node {
-    int node;
-    int weight;
+        return min;
 
-    Node(int node, int weight) {
-        this.node = node;
-        this.weight = weight;
+
+        
     }
 }
